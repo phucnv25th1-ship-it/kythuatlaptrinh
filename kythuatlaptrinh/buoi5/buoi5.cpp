9dfe995a07aec1451b941370fe3e380ea8f9cedf
PMT201 <PMT201@PMT3031>
2026-04-03 11:15:59 +0700

    buoi5

---
+++ kythuatlaptrinh/buoi5/buoi5.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+struct Author {
+    int id;
+    string name;
+};
+
+struct Book {
+    int id;
+    string name;
+    Author  author;
+};
+
+struct Node {
+    Book data;
+    Node* next;
+};
+
+struct LinkedList {
+    Node* head;
+};
+
+int main()
+{
+    LinkedList books = { NULL };
+    do {
+        system("cls");
+        cout << "-------- BOOK MANAGEMENT---------" << endl;
+        cout << "1. Show all books" << endl;
+        cout << "2. Add a book" << endl;cout << "3. Delete a book" << endl;
+        cout << "4. Update a book" << endl;
+        cout << "5. Find book" << endl;
+        cout << "6. Export to file" << endl;
+        cout << "7. Import from file" << endl;
+        cout << "0. Exit" << endl;
+        cout << "----------------------------------" << endl;
+        cout << "Enter your choice: ";
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1: {
+            break;
+        }
+        case 2: {
+            break;
+        }
+        case 3: {
+            break;
+        }
+        case 4: {
+            break;
+        }
+        case 5: {
+            break;
+        }
+        case 6: {
+            break;
+        }
+        case 7: {
+            break;
+        }
+        case 0: {
+            return 0;
+        }
+        default: {
+            cout << "Invalid choice, try again" << endl;
+            break;
+        }
+        }
+        system("pause");
+        cout << "Press any key to continue...";
+    } while (true);0
+}
